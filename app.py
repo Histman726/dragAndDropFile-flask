d9ee941003555d006b086dc7c0f6f29abbe17dfa
Histman7 <hsandovalt1900@alumno.ipn.mx>
2022-12-30 20:45:59 -0600

    Diseño del home y creacion de los archivos perfil y archivos subidos

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def main():
     return render_template('index.html')
 
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     with app.app_context():
